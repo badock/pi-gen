eda6e67899f825f0bb278d61e89bac07fc5d8ad8
badock <jonathan.pastor@me.com>
2019-04-29 13:12:55 +0000

    save work on adding cloud9 to a raspbian image

--- stage2/04-install-cloud9/02-run.sh
+++ stage2/04-install-cloud9/02-run.sh
@@ -3,6 +3,18 @@
 on_chroot << EOF
 #dpkg-reconfigure -f noninteractive tzdata
 #apt-get install -y git nodejs tmux
+
+dpkg-reconfigure locales # add the en_GB.UTF-8 locale and set as default
+export LANGUAGE=en_GB.UTF-8 
+export LANG=en_GB.UTF-8
+export LC_ALL=en_GB.UTF-8
+locale-gen en_GB.UTF-8
+update-locale en_GB.UTF-8
+
+dpkg-reconfigure locales
+nodejs /c9sdk/server.js -l 0.0.0.0 -a name:passwd -w /root
+localedef -f UTF-8 -i en_US en_US.UTF-8
+
 if [ ! -d c9sdk ]; then
    git clone https://github.com/c9/core.git c9sdk
 fi
