4aadfbe073d95f2645a5ae96c49e0a844e2dcf68
badock <jonathan.pastor@me.com>
2019-05-02 23:09:48 +0000

    save work

--- stage2/04-install-cloud9/02-run.sh
+++ stage2/04-install-cloud9/02-run.sh
@@ -12,12 +12,13 @@ locale-gen en_GB.UTF-8
 update-locale en_GB.UTF-8
 
 dpkg-reconfigure locales
-nodejs /c9sdk/server.js -l 0.0.0.0 -a name:passwd -w /root
 localedef -f UTF-8 -i en_US en_US.UTF-8
 
 if [ ! -d c9sdk ]; then
-   git clone https://github.com/c9/core.git c9sdk
+   git clone https://github.com/c9/core.git /var/lib/c9sdk
 fi
-cd c9sdk
+cd /var/lib/c9sdk
 bash scripts/install-sdk.sh
+
+#nodejs /c9sdk/server.js -l 0.0.0.0 -a name:passwd -w /root
 EOF
