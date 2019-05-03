a42cadd428644566cb94a4510018d8e854f660da
root <root@vm-access.novalocal>
2019-05-03 17:19:04 +0000

    add systemd service definition

--- stage2/04-install-cloud9/02-run.sh
+++ stage2/04-install-cloud9/02-run.sh
@@ -20,5 +20,40 @@ fi
 cd /var/lib/c9sdk
 bash scripts/install-sdk.sh
 
-#nodejs /c9sdk/server.js -l 0.0.0.0 -a name:passwd -w /root
+#nodejs /var/lib/c9sdk/server.js -l 0.0.0.0 -a name:passwd -w /root
+
+mkdir /workspace
+
+cat << \EOF > /etc/systemd/system/c9.service
+[Unit]
+Description=Cloud9 IDE
+Requires=network.target
+
+[Service]
+Type=simple
+User=root
+ExecStart=/usr/local/bin/c9
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+\EOF
+
+cat << \EOF > /usr/local/bin/c9
+#!/bin/sh
+USERNAME="pi"
+PASSWORD="raspberry"
+
+nodejs /var/lib/c9sdk/server.js -l 0.0.0.0 \
+    --listen 0.0.0.0 \
+    --port 8181 \
+    -a $USERNAME:$PASSWORD \
+    -w /workspace
+\EOF
+
+systemctl daemon-reload
+systemctl start c9
+systemctl enable c9
+
+
 EOF
