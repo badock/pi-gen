51e888b884291691ae9e8be9c8d5d239a8ad63dc
badock <jonathan.pastor@me.com>
2019-05-12 13:59:02 +0000

    install python debugger

--- stage2/04-install-cloud9/02-run.sh
+++ stage2/04-install-cloud9/02-run.sh
@@ -1,9 +1,6 @@
 #!/bin/bash -e
 
 on_chroot << EOF
-#dpkg-reconfigure -f noninteractive tzdata
-#apt-get install -y git nodejs tmux
-
 dpkg-reconfigure locales # add the en_GB.UTF-8 locale and set as default
 export LANGUAGE=en_GB.UTF-8 
 export LANG=en_GB.UTF-8
@@ -14,7 +11,7 @@ update-locale en_GB.UTF-8
 dpkg-reconfigure locales
 localedef -f UTF-8 -i en_US en_US.UTF-8
 
-if [ ! -d c9sdk ]; then
+if [ ! -d /var/lib/c9sdk ]; then
    git clone https://github.com/c9/core.git /var/lib/c9sdk
 fi
 cd /var/lib/c9sdk
@@ -24,7 +21,7 @@ bash scripts/install-sdk.sh
 
 mkdir /workspace
 
-cat << \EOF > /etc/systemd/system/c9.service
+cat << EOF1 > /etc/systemd/system/c9.service
 [Unit]
 Description=Cloud9 IDE
 Requires=network.target
@@ -37,9 +34,9 @@ Restart=on-failure
 
 [Install]
 WantedBy=multi-user.target
-\EOF
+EOF1
 
-cat << \EOF > /usr/local/bin/c9
+cat << EOF2 > /usr/local/bin/c9
 #!/bin/sh
 USERNAME="pi"
 PASSWORD="raspberry"
@@ -47,13 +44,18 @@ PASSWORD="raspberry"
 nodejs /var/lib/c9sdk/server.js -l 0.0.0.0 \
     --listen 0.0.0.0 \
     --port 8181 \
-    -a $USERNAME:$PASSWORD \
+    -a \$USERNAME:\$PASSWORD \
     -w /workspace
-\EOF
+EOF2
+
+chmod +x /usr/local/bin/c9
 
 systemctl daemon-reload
 systemctl start c9
 systemctl enable c9
 
+# Install packages for enabling python debugger in Cloud9
+apt install -y python-dev python-pip
+pip install ikpdb
 
 EOF
